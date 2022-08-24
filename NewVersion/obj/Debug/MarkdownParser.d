06b282838c0f4f2f90488f21edbb6069b05bfe34
Eessh <76767406+Eessh@users.noreply.github.com>
2022-08-24 21:31:26 +0530

    Delete MarkdownParser.d

--- NewVersion/obj/Debug/MarkdownParser.d
+++
@@ -1,9 +0,0 @@
-obj/Debug/MarkdownParser.o: MarkdownParser.cpp include/MarkdownDoc.hpp \
- include/Lexer.hpp include/Token.hpp include/TokenTypes.hpp \
- include/AbstractSyntaxTree.hpp include/AbstractSyntaxTreeNode.hpp
-include/MarkdownDoc.hpp:
-include/Lexer.hpp:
-include/Token.hpp:
-include/TokenTypes.hpp:
-include/AbstractSyntaxTree.hpp:
-include/AbstractSyntaxTreeNode.hpp:
